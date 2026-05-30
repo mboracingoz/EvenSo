a4832440ab604b64176bd8465cdd14ded808613a
mboracingoz <mboracingoz@gmail.com>
2026-05-30 10:56:04 -0400

    init push

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.title = "Even So"
+    t.version = "11.4"
+
+    t.window.width = 1920
+    t.window.height = 1080
+    t.window.getFullscreen = false
+    t.window.vsync = 1
+    t.window.resizable = false
+end
---
+++ main.lua
@@ -0,0 +1,12 @@
+function love.load()
+
+end
+
+function love.update()
+    
+end
+
+
+function love.draw()
+    love.graphics.print("Even So çalışıyor!!!", 20,20)
+end
